50c9457a7822f0663b6968b4333072832ed4eab6
Alex Walhout <alexpwalhout@gmail.com>
2019-11-10 18:25:55 -0800

    working on theme logic
    hit a snag with how fonts must be made new in order to change aspects. so would have to sepparate by h1, h2 as can't assign just the color to a font.

--- DataScreen/MainWindow.cs
+++ DataScreen/MainWindow.cs
@@ -12,7 +12,10 @@ namespace DataScreen
 {
     public partial class MainWindow : Form
     {
-        Array textObjects;
+        Label[] textObjects;
+        byte zero = 0;
+
+        Font defaultFont = new Font("Arial", 9, FontStyle.Regular);
 
         public MainWindow()
         {
@@ -22,7 +25,25 @@ namespace DataScreen
         private void Form1_Load(object sender, EventArgs e)
         {
             //add all text objects to a text object array so they can be updated for 
-            textObjects.SetValue(labelTitle, 0);
+            //!!! number of items in textObjects cannot exceed a byte !!!!!!
+            //textObjects.SetValue(labelTitle, 0);
+            //load the default theme:::future extensibility, load from file so theme can be saved
+            LoadTheme(zero);
+        }
+
+        //loads the called selection. byte keeps memory storage small
+        private void LoadTheme(byte selection)
+        {
+            //switch case based on selection will call theme adjustments
+            switch(selection)
+            {
+                default:
+                    /*foreach(Label label in textObjects)
+                    {
+                        label.Font = defaultFont;
+                    }*/
+                    break;
+            }
         }
 
         private void labelTitle_Click(object sender, EventArgs e)

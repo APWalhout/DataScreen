e36f39f1179bc41a4fc21e5fc7ba7e80fbf2dce4
Alex Walhout <alexpwalhout@gmail.com>
2019-11-10 17:36:03 -0800

    Renamed Form1 to MainWindow

--- DataScreen/MainWindow.Designer.cs
+++ DataScreen/MainWindow.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DataScreen
 {
-    partial class Form1
+    partial class MainWindow
     {
         /// <summary>
         /// Required designer variable.
@@ -28,21 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelTitle = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Location = new System.Drawing.Point(13, 13);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(73, 13);
+            this.labelTitle.TabIndex = 0;
+            this.labelTitle.Text = "Project Status";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(906, 613);
+            this.Controls.Add(this.labelTitle);
             this.Name = "Form1";
             this.Text = "Hello World";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelTitle;
     }
 }
 
